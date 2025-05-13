f94abbfc53aaf175c1b85dfad63bd824cd7e5220
Abu Salehin Rifat <salehinrifat2@gmail.com>
2025-05-14 00:21:28 +0600

    Added Quick sort

---
+++ Codes/Sorting/merege.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(v) v.begin(), v.end()
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        }
+    return 0;
+}
